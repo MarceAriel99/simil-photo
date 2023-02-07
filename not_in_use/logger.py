fac66d1e5b818bf6a06ecd370bb86b13ecde986a
MarceAriel99 <mrondan@fi.uba.ar>
2023-02-07 10:38:20 -0300

    Deleted unused files

--- not_in_use/logger.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-CURRENT_DIRECTORY = 'E:/Mis_Archivos/Proyects/Programs/SimilPhoto/Program/'
-LOGS_PATH = 'logs.txt'
-
-def log(message):
-	
-	with open (CURRENT_DIRECTORY+LOGS_PATH, "a+") as file:
-		file.write(datetime.today().strftime("%d/%m/%Y-%X") + " - " + message + "\n")
